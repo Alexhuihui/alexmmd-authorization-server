4621ca6284661c81384c86431ef561d92ba77f8a
Alex <2930807240@qq.com>
2022-02-21 14:41:20 +0800

    Initial commit

---
+++ src/main/java/top/alexmmd/authorization/AlexmmdAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package top.alexmmd.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexmmdAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlexmmdAuthorizationServerApplication.class, args);
+    }
+
+}
